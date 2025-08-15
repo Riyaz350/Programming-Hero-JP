136b9adc5890106fac27667b63d1eaa80335426f
unknown <riyaz3582@gmail.com>
2025-08-15 20:28:54 +0600

    changed cors, vercel.json

--- college-booking-backend/api/index.js
+++ college-booking-backend/api/index.js
@@ -1,17 +1,9 @@
-// index.js
-import app from "./app.js";
-import dotenv from "dotenv";
-import serverless from "serverless-http";
-import dbConnect from "./config/db.js";
+import app from './app.js';
+import dotenv from 'dotenv';
+import serverless from 'serverless-http';
+import dbConnect from './config/db.js';
 
 dotenv.config();
-
-// Connect to DB once per lambda cold start
 await dbConnect();
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
 
 export const handler = serverless(app);
