faf61abc139de63d7e996325fa0db6fb10a9d61a
unknown <riyaz3582@gmail.com>
2025-08-15 11:58:33 +0600

    changed api

--- college-booking-frontend/src/hook/useAxiosPublic.jsx
+++ college-booking-frontend/src/hook/useAxiosPublic.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 const useAxiosPublic = () => {
     const axiosPublic = useMemo(() => {
         return axios.create({
-            // baseURL: 'https://programming-hero-jp-vhgk.vercel.app/api/v1'
-            baseURL: 'http://localhost:5000/api/v1'
+            baseURL: 'https://programming-hero-jp-vhgk.vercel.app/api/v1'
+            // baseURL: 'http://localhost:5000/api/v1'
         });
     }, []);
     return axiosPublic;
